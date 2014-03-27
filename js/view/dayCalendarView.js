857037b533dd41607423d9296b8632691738e699
aw-jansen <a.w.jansen.1988@gmail.com>
2014-03-27 18:41:50 +0100

    legend style change v2

--- js/view/dayCalendarView.js
+++ js/view/dayCalendarView.js
@@ -55,8 +55,8 @@ var DayCalendarView = function(container,model)
 			displayLegend: true,
 			legendVerticalPosition: "top",
 			legendHorizontalPosition: "left",
-			legendCellSize: 30,
-			legendMargin:[-10,0,12,577],
+			legendCellSize: 27,
+			legendMargin:[-10,0,12,592],
 			legend: [Math.round(max*0.2),Math.round(max*0.4),Math.round(max*0.6),Math.round(max*0.8)],
 			onClick: function(date,value,rect)
 			{
